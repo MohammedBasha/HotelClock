bb6482b7d9fa5efe6657d954ccdc7c6b42b8952e
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-09 14:11:58 +0200

    Adjust the Clock Design

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,21 @@
         function updateTime() {
             var d = new Date(),
                 hours = d.getHours(),
-                sep = (d.getSeconds() % 2 === 0)? ":" : " ";
+                minutes = d.getMinutes(),
+                sepClass = (d.getSeconds() % 2 === 0) ? "" : "trans",
+                sep = "<span class='" + sepClass + "'>:</span>",
+                am_pm = (hours > 12) ? "PM" : "AM";
 
-            if (hours > 12) hours -= 12;
+            if (hours > 12) {
+                hours -= 12;
+            } else if (hours === 12) {
+                hours = 12;
+            }
+            
+            hours = (hours < 10) ? "0" + hours : hours;
+            minutes = (minutes < 10) ? "0" + minutes : minutes;
 
-            c.innerHTML = hours + sep + d.getMinutes();
+            c.innerHTML = hours + sep + minutes + " " + am_pm;
         }
 
         setInterval(updateTime, 1000);
