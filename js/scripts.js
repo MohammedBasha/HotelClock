1cc232b1a079e73af0206c2c2587668d31d7176b
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-09 10:06:18 +0200

    Update the time

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 (function () {
     "use strict";
-    
+
     document.addEventListener("DOMContentLoaded", function () {
         const c = document.getElementById("current-time");
-        var d = new Date(),
-            hours = d.getHours();
-        
-        if (hours > 12) hours -= 12;
-        c.innerHTML = hours + ":" + d.getMinutes();
+
+        function updateTime() {
+            var d = new Date(),
+                hours = d.getHours(),
+                sep = (d.getSeconds() % 2 === 0)? ":" : " ";
+
+            if (hours > 12) hours -= 12;
+
+            c.innerHTML = hours + sep + d.getMinutes();
+        }
+
+        setInterval(updateTime, 1000);
+
     });
 
 }());
